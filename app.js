315a94dbfceb143f20bd9f22aed9cfeb1d574e51
Dohae <dode777.dev@gmail.com>
2021-02-16 19:40:57 +0900

    📝 Docs: 테스트 클라이언트 폴더 경로 지정

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const { join } = require('path');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const chatRouter = require('./chatRouter');
 
 app.use(morgan('dev'));
+app.use(express.static(join(__dirname, 'tempClient')));
 
 app.use('/', chatRouter);
 
