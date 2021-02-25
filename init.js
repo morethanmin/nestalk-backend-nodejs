240b85286ea19009392177dd62a72c8399472453
Dohae <dode777.dev@gmail.com>
2021-02-25 13:16:34 +0900

    📝 Docs: IP 정보 추가, 연결해제 구문 추가

--- init.js
+++ init.js
@@ -14,16 +14,23 @@ const server = app.listen(PORT, handleListening);
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log(`✅ Server connected successfully`);
+  // IP 정보
+  const req = socket.request;
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`✅ Server connected successfully`, ip, socket.id);
+  // 클라이언트로부터 메시지 수신
   socket.on('newMessage', ({ message }) => {
     socket.broadcast.emit('messageNotification', { message, nickname: socket.nickname || 'Anon' });
   });
+  // 에러
   socket.on('error', (error) => {
     console.log(error);
   });
-  socket.on('close', () => {
-    console.log('클라이언트 접속 해제', ip);
+  // 연결 종료
+  socket.on('disconnect', () => {
+    console.log('❌ 클라이언트 접속 해제', ip, socket.id);
   });
+  // 닉네임 설정
   socket.on('setNickname', ({ nickname }) => {
     socket.nickname = nickname;
   });
