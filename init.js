6afaefef58f242eeabb2fd945bad45ef6dc9241e
2langk <65915933+2langk@users.noreply.github.com>
2021-03-13 17:03:14 +0900

    콘솔 로그로 채팅 기능 구현 (#12)

--- init.js
+++ init.js
@@ -8,31 +8,38 @@ const PORT = process.env.PORT || 3000;
 
 // Listen Port
 function handleListening() {
-  console.log(`✅ Listening at: http://localhost:${PORT}`);
+    console.log(`✅ Listening at: http://localhost:${PORT}`);
 }
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  // IP 정보
-  const req = socket.request;
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(`✅ Server connected successfully`, ip, socket.id);
-  // 클라이언트로부터 메시지 수신
-  socket.on('newMessage', ({ message }) => {
-    socket.broadcast.emit('messageNotification', { message, nickname: socket.nickname || 'Anon' });
-  });
-  // 에러
-  socket.on('error', (error) => {
-    console.log(error);
-  });
-  // 연결 종료
-  socket.on('disconnect', () => {
-    console.log('❌ 클라이언트 접속 해제', ip, socket.id);
-  });
-  // 닉네임 설정
-  socket.on('setNickname', ({ nickname }) => {
-    socket.nickname = nickname;
-  });
+    // 유저가 채팅방에 입장한 경우
+    socket.on('userJoin', ({userId, roomId}) => {
+        // 채팅방에 참여중인 인원 정보 + 이전 채팅 내역 전달.
+        const roomUsers = [
+            {id: 2, roomId, name: 'kim'},
+            {id: 3, roomId, name: 'lee'},
+            {id: 4, roomId, name: 'park'},
+        ];
+
+        const roomChats = [
+            {id: 1, roomId, userId: 1, message: 'Hello! - 1'},
+            {id: 1, roomId, userId: 2, message: 'Hello! - 2'},
+            {id: 1, roomId, userId: 3, message: 'Hello! - 3'},
+            {id: 1, roomId, userId: 4, message: 'Hello! - 4'},
+        ];
+
+        socket.leaveAll();
+        socket.join(roomId);
+
+        socket.emit('roomInfo', {roomUsers, roomChats});
+    });
+
+    // 유저가 메시지를 전송한 경우
+    socket.on('sendMessageToServer', ({userId, roomId, message}) => {
+        // DB에 채팅내용 저장하고 채팅방에 전달
+        io.to(roomId).emit('getMessageFromServer', {userId, roomId, message});
+    });
 });
--- tempClient/index.js
+++ tempClient/index.js
@@ -1,17 +1,46 @@
-const socket = io('/');
+const socket = io('http://localhost:3000');
 
-function sendMessage(message) {
-  socket.emit('newMessage', { message });
-  console.log(`You: ${message}`);
-}
+const joinForm = document.getElementById('joinForm');
+const chatForm = document.getElementById('chatForm');
 
-function setNickname(nickname) {
-  socket.emit('setNickname', { nickname });
-}
+let userId;
+let roomId;
 
-function handleMessageNotification(data) {
-  const { message, nickname } = data;
-  console.log(`${nickname}: ${message}`);
-}
+// 채팅방 입장 버튼을 누르면
+joinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    userId = document.getElementById('userId').value;
+    roomId = document.getElementById('roomId').value;
 
-socket.on('messageNotification', handleMessageNotification);
+    // 유저가 채팅방에 입장.
+    socket.emit('userJoin', {userId, roomId});
+});
+
+// 채팅방 정보를 받아옴.
+socket.on('roomInfo', ({roomUsers, roomChats}) => {
+    console.log('채팅에 참여중인 인원 (가짜 데이터)');
+    roomUsers.forEach((user) => {
+        console.log(user);
+    });
+
+    console.log('---');
+    console.log('이전 채팅 내역 (가짜 데이터)');
+    roomChats.forEach((chat) => {
+        console.log(chat);
+    });
+});
+
+// 메시지 전송버튼을 누르면
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    userId = document.getElementById('userId').value;
+    roomId = document.getElementById('roomId').value;
+    const message = document.getElementById('chatInput').value;
+
+    socket.emit('sendMessageToServer', {userId, roomId, message});
+});
+
+// 서버로부터 메시지를 받음
+socket.on('getMessageFromServer', ({userId, roomId, message}) => {
+    console.log({userId, roomId, message});
+});
